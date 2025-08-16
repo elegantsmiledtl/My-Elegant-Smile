10c60b318514227d9e81672cfc44016cd3b4d690
Ahmad Mishael <elegantsmiledtl@gmail.com>
2025-08-16 10:40:26 +0000

    Dental Lab font color same Elegant Smile color

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
   return (
     <Link href="/" className="flex flex-col items-start">
       <h1 className="text-2xl font-bold font-headline text-primary leading-tight">Elegant Smile</h1>
-      <p className="text-xs text-muted-foreground -mt-1 ml-1">Dental Lab</p>
+      <p className="text-xs text-primary -mt-1 ml-1">Dental Lab</p>
     </Link>
   );
 }
