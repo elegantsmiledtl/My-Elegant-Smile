ba10fe2c0b6f411e96af2d1a394ce10626633828
Ahmad Mishael <elegantsmiledtl@gmail.com>
2025-09-06 12:37:04 +0000

    replace logo with Back button

--- src/app/doctor/[dentistName]/page.tsx
+++ src/app/doctor/[dentistName]/page.tsx
@@ -142,7 +142,6 @@ export default function DoctorPage() {
             </h2>
           </div>
           <div className="flex justify-end">
-            <Logo />
           </div>
         </div>
       </header>
