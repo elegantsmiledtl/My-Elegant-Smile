2a66d03e1667e746a003ca9f9b0322af0217ce1a
Ahmad Mishael <elegantsmiledtl@gmail.com>
2025-09-08 12:26:10 +0000

    change from Enter Lab Portal to Lab User

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function HomePage() {
           </div>
           <div className="flex flex-col items-center gap-4 w-full max-w-xs">
             <Button asChild size="lg" className="w-full bg-[#C6A963] hover:bg-[#C6A963]/90 text-white font-bold">
-              <Link href="/owner">Enter Lab Portal</Link>
+              <Link href="/owner">Lab User</Link>
             </Button>
              <Button asChild size="lg" className="w-full bg-[#C6A963] hover:bg-[#C6A963]/90 text-white font-bold">
               <Link href="/login">Doctor User</Link>
